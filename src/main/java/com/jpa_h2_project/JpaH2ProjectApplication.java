25f5190dd0cd46630b6e7a7658ad198770425605
batsyKun <119741750+batsyKun@users.noreply.github.com>
2026-03-06 10:17:14 +0530

    initialize Spring Boot project using Spring Initializr

---
+++ src/main/java/com/jpa_h2_project/JpaH2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jpa_h2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa_h2_project/JpaH2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa_h2_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
